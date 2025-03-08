f156658a2b86583cc4535991c5fd4989205e3445
Amit Kushwaha <amitcoding56@gmail.com>
2025-03-08 15:28:08 +0530

    navbar done

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <div className="flex justify-between items-center bg-black px-16 py-9">
+      <Link to="/">
+        <div className="text-2xl font-bold">Bloggy</div>
+      </Link>
+      <nav className="flex justify-center items-center gap-5">
+        <ul className="flex justify-center items-center gap-5">
+          <Link to="/">
+            <li>Home</li>
+          </Link>
+          <Link to="/about">
+            <li>About</li>
+          </Link>
+          <Link to="/blogs">
+            <li>Blogs</li>
+          </Link>
+          <Link to="/services">
+            <li>Services</li>
+          </Link>
+          <Link to="/contact">
+            <li>Contact</li>
+          </Link>
+        </ul>
+        <button className="bg-green-500 rounded-md py-2 px-4 cursor-pointer">
+          Logout
+        </button>
+      </nav>
+    </div>
+  );
 }
 
-export default Navbar
+export default Navbar;
